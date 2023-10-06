0dfcfddf452a66bcf77aa27c4a4082057c69d0fa
Mohamed Almahdy <engmohamedlmahdy@gmail.com>
2023-10-06 11:50:24 +0300

    Created Home

--- core/src/commonMain/kotlin/com/lightfeather/core/domain/transaction/Transaction.kt
+++ core/src/commonMain/kotlin/com/lightfeather/core/domain/transaction/Transaction.kt
@@ -4,21 +4,30 @@ import com.lightfeather.core.domain.Account
 import com.lightfeather.core.domain.Category
 
 sealed class Transaction(
-    val id: Int, val name: String, val description: String?, val categories: List<Category>,
-    val amount: Double, val timestamp: Long, val account: Account
+    val id: Int,
+    val name: String,
+    val description: String?,
+    val amount: Double,
+    val timestamp: Long,
+    val account: Account
 ) {
+
     class Income(
-        id: Int, name: String, description: String?, category: List<Category>,
+        id: Int, name: String, description: String?,
         amount: Double, timestamp: Long, account: Account, val source: String
-    ) : Transaction(id, name, description, category, amount, timestamp, account)
+    ) : Transaction(id, name, description, amount, timestamp, account)
+
 
     class Expense(
-        id: Int, name: String, description: String?, category: List<Category>,
+        id: Int, name: String, description: String?, categories: List<Category>,
         amount: Double, timestamp: Long, account: Account
-    ) : Transaction(id, name, description, category, amount, timestamp, account)
+    ) : Transaction(id, name, description, amount, timestamp, account)
+
 
     class Transfer(
-        id: Int, name: String, description: String?, category: List<Category>,
-        amount: Double, timestamp: Long, account: Account, val receiverAccount : Account, val fee: Double
-    ) : Transaction(id, name, description, category, amount, timestamp, account)
+        id: Int, name: String, description: String?,
+        amount: Double, timestamp: Long, account: Account,
+        val receiverAccount: Account, val fee: Double
+    ) : Transaction(id, name, description, amount, timestamp, account)
+
 }
